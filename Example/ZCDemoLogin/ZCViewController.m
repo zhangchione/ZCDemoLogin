9964c3e30872d8e607ffa38b45b2db89920ffd07
zhangchi06 <zhangchi06@kuaishou.com>
2020-05-22 16:36:35 +0800

    login

---
+++ Example/ZCDemoLogin/ZCViewController.m
@@ -0,0 +1,29 @@
+//
+//  ZCViewController.m
+//  ZCDemoLogin
+//
+//  Created by zhangchione on 05/22/2020.
+//  Copyright (c) 2020 zhangchione. All rights reserved.
+//
+
+#import "ZCViewController.h"
+
+@interface ZCViewController ()
+
+@end
+
+@implementation ZCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
